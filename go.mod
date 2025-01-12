10eca4561183bcb65c4233e7c9274472e4318f5b
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2025-01-12 00:23:44 -0800

    chore: add packages to local directory

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aaronang/raft-go
+
+go 1.20
--- raft.go
+++ raft.go
@@ -24,12 +24,9 @@ import (
 	"sync/atomic"
 	"time"
 
-	"cs350/labgob"
-	"cs350/labrpc"
+	"github.com/aaronang/raft-go/pkg/labgob"
+	"github.com/aaronang/raft-go/pkg/labrpc"
 )
-
-// import "bytes"
-// import "cs350/labgob"
 
 // as each Raft peer becomes aware that successive log entries are
 // committed, the peer should send an ApplyMsg to the service (or
