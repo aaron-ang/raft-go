86a6856dd577b78b0bd0e9f75af971743f13a236
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2025-01-12 21:43:21 -0800

    refactor: update utils

--- util.go
+++ util.go
@@ -5,10 +5,6 @@ import "log"
 // Debugging
 const Debug = false
 
-func init() {
-	log.SetFlags(log.Lmicroseconds)
-}
-
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug {
 		log.Printf(format, a...)
